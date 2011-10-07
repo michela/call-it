60650063085fcf4aab314f94cc76b9d50fd8b605
Michela Ledwidge <michela@thequality.com>
2011-10-07 23:53:33 +1100

    change port again

--- app.js
+++ app.js
@@ -105,5 +105,5 @@ app.get('/facebook', function(req, res){
   });
 });
 
-app.listen(5000);
+app.listen(30699);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
